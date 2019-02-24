90eb58d5b0b0d63e3d714c9e45622332543575e1
FlutterAddict <johnnydevx@gmail.com>
2019-02-24 18:38:56 +0100

    Add 'dart/double-it' (to be continued)

--- js/data.js
+++ js/data.js
@@ -2,6 +2,7 @@ export default {
   dart: [
     { key: 'dart/0-Intro', label: 'Dart Introduction', active: true },
     { key: 'dart/1-Hello-World', label: 'Hello World Program' },
+    { key: 'dart/2-Double-It', label: 'Double It Program' },
   ],
   flutter: [
     { key: 'flutter/0-Welcome', label: 'Welcome to Flutter Studio', active: true },
