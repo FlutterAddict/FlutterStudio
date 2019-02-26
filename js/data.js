3b0ad9fc2781425f54a59d75794a58e95da71d18
FlutterAddict <johnnydevx@gmail.com>
2019-02-26 17:15:28 +0100

    Add flutter/Basic-Widgets; Rename Lessons; Reorganize Content Structure

--- js/data.js
+++ js/data.js
@@ -5,9 +5,11 @@ export default {
     { key: 'dart/2-Concepts', label: 'Important Concepts' },
   ],
   flutter: [
-    { key: 'flutter/0-Welcome', label: 'Welcome to Flutter Studio', active: true },
-    { key: 'flutter/1-Why-Flutter', label: 'Why Flutter?' },
-    { key: 'flutter/2-Hello-World', label: 'Hello World App' },
+    { key: 'flutter/Welcome', label: 'Welcome to Flutter Studio', active: true },
+    { key: 'flutter/Why-Flutter', label: 'Why Flutter?' },
+    { key: 'flutter/Hello-World', label: 'Hello World App' },
+    { key: 'flutter/Basic-Widgets', label: 'Basic Widgets' },
+    { key: 'flutter/Custom-Widgets', label: 'Custom Widgets' },
   ],
   blog: [
     { key: 'blog/flutter-vs-rn', label: 'Flutter vs. React Native', active: true },
