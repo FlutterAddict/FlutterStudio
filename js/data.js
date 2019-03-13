eed0b01113bf9c7888bcb083a9905d39d7b07a27
FlutterAddict <johnnydevx@gmail.com>
2019-03-14 00:07:18 +0100

    Directory structure improvements

--- js/data.js
+++ js/data.js
@@ -1,9 +1,9 @@
 export default {
   dart: {
     'Foundation': [
-      { key: 'dart/0-Intro', label: 'Introduction', active: true },
-      { key: 'dart/1-Hello-World', label: 'Hello World', program: true },
-      { key: 'dart/2-Concepts', label: 'Important Concepts' },
+      { key: 'dart/foundation/0-Intro', label: 'Introduction', active: true },
+      { key: 'dart/foundation/1-Hello-World', label: 'Hello World', program: true },
+      { key: 'dart/foundation/2-Concepts', label: 'Important Concepts' },
     ]
   },
   flutter: {
